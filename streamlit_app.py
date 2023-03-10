d2643bf96a2d7c485b1d305b873d92506e96f6a9
Samuel_DS <62851341+samueldsingh@users.noreply.github.com>
2023-03-10 16:36:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,102 +1,97 @@
-# Data Source: https://public.tableau.com/app/profile/federal.trade.commission/viz/FraudandIDTheftMaps/AllReportsbyState
-# US State Boundaries: https://public.opendatasoft.com/explore/dataset/us-state-boundaries/export/
-
 import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
 
-APP_TITLE = 'Fraud and Identity Theft Report'
-APP_SUB_TITLE = 'Source: Federal Trade Commission'
+APP_TITLE = 'Agriculture Crop Production in India'
+APP_SUB_TITLE = 'Source: Ministry of Agriculture and Farmers Welfare of India'
 
-def display_time_filters(df):
-    year_list = list(df['Year'].unique())
-    year_list.sort()
-    year = st.sidebar.selectbox('Year', year_list, len(year_list)-1)
-    quarter = st.sidebar.radio('Quarter', [1, 2, 3, 4])
-    st.header(f'{year} Q{quarter}')
-    return year, quarter
+def total_yield(df, year, season, state, field_name, crop, metric_title):
+    df = df[(df['Year'] == year) & (df['Season'] == season) & (df['Crop'] == crop)]
+    if state:
+        df = df[df['State'] == state]
+    df.drop_duplicates(inplace=True)
+    total = df[field_name].sum()
+    st.metric(metric_title, '{:,}'.format(round(total)))
 
-def display_state_filter(df, state_name):
-    state_list = [''] + list(df['State Name'].unique())
-    state_list.sort()
-    state_index = state_list.index(state_name) if state_name and state_name in state_list else 0
-    return st.sidebar.selectbox('State', state_list, state_index)
+def display_map(df, year, crop, season):
+    df = df[(df['Year'] == year) & (df['Season'] == season) & (df['Crop'] == crop)]
 
-def display_report_type_filter():
-    return st.sidebar.radio('Report Type', ['Fraud', 'Other'])
+    map = folium.Map(location=[20,80], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
 
-def display_map(df, year, quarter):
-    df = df[(df['Year'] == year) & (df['Quarter'] == quarter)]
-
-    map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
-    
     choropleth = folium.Choropleth(
-        geo_data='data/us-state-boundaries.geojson',
-        data=df,
-        columns=('State Name', 'State Total Reports Quarter'),
-        key_on='feature.properties.name',
+        geo_data = 'data/states_india.geojson',
+        data = df,
+        columns=('State', 'Yield'),
+        key_on= 'feature.properties.st_nm',
         line_opacity=0.8,
-        highlight=True
+        highlight=True   
     )
     choropleth.geojson.add_to(map)
 
-    df_indexed = df.set_index('State Name')
-    for feature in choropleth.geojson.data['features']:
-        state_name = feature['properties']['name']
-        feature['properties']['population'] = 'Population: ' + '{:,}'.format(df_indexed.loc[state_name, 'State Pop'][0]) if state_name in list(df_indexed.index) else ''
-        feature['properties']['per_100k'] = 'Reports/100K Population: ' + str(round(df_indexed.loc[state_name, 'Reports per 100K-F&O together'][0])) if state_name in list(df_indexed.index) else ''
-
     choropleth.geojson.add_child(
-        folium.features.GeoJsonTooltip(['name', 'population', 'per_100k'], labels=False)
+        folium.features.GeoJsonTooltip(['st_nm'], labels=False)
     )
-    
-    st_map = st_folium(map, width=700, height=450)
 
-    state_name = ''
+    st_map = st_folium(map, width=700, height=500)
+
+    state = ''
     if st_map['last_active_drawing']:
-        state_name = st_map['last_active_drawing']['properties']['name']
-    return state_name
+        state = st_map['last_active_drawing']['properties']['st_nm']
 
-def display_fraud_facts(df, year, quarter, report_type, state_name, field, title, string_format='${:,}', is_median=False):
-    df = df[(df['Year'] == year) & (df['Quarter'] == quarter)]
-    df = df[df['Report Type'] == report_type]
-    if state_name:
-        df = df[df['State Name'] == state_name]
-    df.drop_duplicates(inplace=True)
-    if is_median:
-        total = df[field].sum() / len(df[field]) if len(df) else 0
-    else:
-        total = df[field].sum()
-    st.metric(title, string_format.format(round(total)))
+    return state
+
+def display_time_filters(df):
+    year_list = [''] + list(df['Year'].unique())
+    year_list.sort(reverse=True)
+    year = st.sidebar.selectbox('Year', year_list)
+    season_list = [''] + list(df['Season'].unique())
+    season = st.sidebar.selectbox('Season', season_list)
+    st.header(f'{year} {season}')
+    return year, season
+
+def crop_filter(df):
+    crop_list = [''] + list(df['Crop'].unique())
+    crop_list.sort()
+    return st.sidebar.selectbox('Crop', crop_list)
+
+def display_state_filter(df, state):
+    state_list = [''] + list(df['State'].unique())
+    state_list.sort()
+    state_index = state_list.index(state) if state and state in state_list else 0
+    return st.sidebar.selectbox('State', state_list, state_index)
 
 def main():
     st.set_page_config(APP_TITLE)
     st.title(APP_TITLE)
     st.caption(APP_SUB_TITLE)
 
-    #Load Data
-    df_continental = pd.read_csv('data/continental.csv')
-    df_fraud = pd.read_csv('data/fraud.csv')
-    df_median = pd.read_csv('data/median.csv')
-    df_loss = pd.read_csv('data/losses.csv')
+    #LOAD DATA
+    df_country = pd.read_csv('India Agriculture Crop Production.csv')   
+    df_area = pd.read_csv('India Agriculture Crop Production.csv')
+    df_prod = pd.read_csv('India Agriculture Crop Production.csv')
+    df = pd.read_csv('India Agriculture Crop Production.csv')
 
-    #Display Filters and Map
-    year, quarter = display_time_filters(df_continental)
-    state_name = display_map(df_continental, year, quarter)
-    state_name = display_state_filter(df_continental, state_name)
-    report_type = display_report_type_filter()
+    field_name = 'Yield'
+    crop = 'Rice'
+ 
+    #DISPLAY FILTERS AND MAP
+    year, season = display_time_filters(df_country)
+    state = display_map(df_country, year, crop, season)
+    crop = crop_filter(df_country)
+    state = display_state_filter(df_country, state)
 
-    #Display Metrics
-    st.subheader(f'{state_name} {report_type} Facts')
 
+
+    #DISPLAY METRICS
+    st.subheader(f'{state} Facts')
     col1, col2, col3 = st.columns(3)
     with col1:
-        display_fraud_facts(df_fraud, year, quarter, report_type, state_name, 'State Fraud/Other Count', f'# of {report_type} Reports', string_format='{:,}')
+        total_yield(df_area, year, season, state, 'Area', crop, 'Total Area (Hectares)')
     with col2:
-        display_fraud_facts(df_median, year, quarter, report_type, state_name, 'Overall Median Losses Qtr', 'Median $ Loss', is_median=True)
+        total_yield(df_prod, year, season, state, 'Production', crop, 'Total Production (Tonnes)')
     with col3:
-        display_fraud_facts(df_loss, year, quarter, report_type, state_name, 'Total Losses', 'Total $ Loss')        
+        total_yield(df, year, season, state, field_name, crop, f'Total {field_name}')
 
 
 if __name__ == "__main__":
